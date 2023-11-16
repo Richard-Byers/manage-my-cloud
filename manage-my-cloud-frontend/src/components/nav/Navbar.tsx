8643738616e96e47d8675c3681e3d78c48edb0c7
rbyers <rbyers800@gmail.com>
2023-11-16 18:00:48 +0000

    Brought over blue rectangle on top of navbar and added in logo tied to dashboard.

--- manage-my-cloud-frontend/src/components/nav/Navbar.tsx
+++ manage-my-cloud-frontend/src/components/nav/Navbar.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
+import logo from '../images/managemycloudlogo.svg';
 
 const Navbar: React.FC = () => {
     return (
         <nav className="navbar">
             <ul className="navbar-nav">
+                <li className="logo">
+                    <Link to="/" className="nav-link-logo">
+                        <img src={logo} alt="manage my cloud logo" height="150" width="150" />
+                    </Link>
+                </li>
                 <li className="nav-item">
                     <Link to="/" className="nav-link">
                         Dashboard
@@ -24,6 +30,16 @@ const Navbar: React.FC = () => {
                 <li className="nav-item">
                     <Link to="/profile" className="nav-link">
                         Profile
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/settings" className="nav-link">
+                        Settings
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link to="/sign-out" className="nav-link">
+                        Sign Out
                     </Link>
                 </li>
             </ul>
