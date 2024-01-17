d7a7c43eb525d0bae02864abe6521946f1b224b8
Richard Byers <rbyers03@qub.ac.uk>
2024-01-17 21:39:03 +0000

    Added in Unit test for OAuthAuthorisationControllerTest, may need expanded upon in future but good starting point showing a 200 returned, hard to mock out the Google part without putting in actual account. Also edited 2 other tests as due to the accountType and refreshToken being added, I made those values null and they pass now

--- manage-my-cloud-backend/authorisation-service/src/test/java/com/authorisation/givens/UserEntityGivens.java
+++ manage-my-cloud-backend/authorisation-service/src/test/java/com/authorisation/givens/UserEntityGivens.java
@@ -5,11 +5,11 @@ import com.authorisation.entities.UserEntity;
 public class UserEntityGivens {
 
     public static UserEntity generateUserEntity() {
-        return new UserEntity(1L, "John", "Doe", "johndoe@gmail.com", "password", "USER", false);
+        return new UserEntity(1L, "John", "Doe", "johndoe@gmail.com", "password", "USER", false, null, null, null);
     }
 
     public static UserEntity generateUserEntityEnabled() {
-        return new UserEntity(1L, "John", "Doe", "johndoe@gmail.com", "password", "USER", true);
+        return new UserEntity(1L, "John", "Doe", "johndoe@gmail.com", "password", "USER", true, null, null, null);
     }
 
 }
