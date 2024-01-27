5338931fbb13c812cf71ad115110a7dd008b8bd1
Richard Byers <rbyers03@qub.ac.uk>
2024-01-27 13:52:33 +0000

    Split GoogleAuthController process into separate service

---
+++ manage-my-cloud-backend/authorisation-service/src/main/java/com/authorisation/services/GoogleAuthService.java
@@ -0,0 +1,69 @@
+package com.authorisation.services;
+
+import com.authorisation.config.UserAuthenticationProvider;
+import com.authorisation.dto.UserDto;
+import com.google.api.client.googleapis.auth.oauth2.*;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStreamReader;
+
+@Service
+public class GoogleAuthService {
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private UserAuthenticationProvider userAuthenticationProvider;
+
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
+
+    public ResponseEntity<UserDto> storeAuthCode(String authCode) {
+        String jsonString = authCode.substring(authCode.indexOf("{"));
+        JSONObject jsonObject = new JSONObject(jsonString);
+        String authCodeOutput = jsonObject.getString("authCode");
+
+        try {
+            GoogleClientSecrets clientSecrets =
+                    GoogleClientSecrets.load(
+                            JacksonFactory.getDefaultInstance(), new InputStreamReader(getClass().getResourceAsStream(CREDENTIALS_FILE_PATH)));
+            GoogleTokenResponse tokenResponse =
+                    new GoogleAuthorizationCodeTokenRequest(
+                            new NetHttpTransport(),
+                            JacksonFactory.getDefaultInstance(),
+                            "https://www.googleapis.com/oauth2/v4/token",
+                            clientSecrets.getDetails().getClientId(),
+                            clientSecrets.getDetails().getClientSecret(),
+                            authCodeOutput,
+                            "postmessage")
+                            .execute();
+
+            String refreshToken = tokenResponse.getRefreshToken();
+
+            String idTokenStr = tokenResponse.getIdToken();
+            GoogleIdToken idToken = GoogleIdToken.parse(JacksonFactory.getDefaultInstance(), idTokenStr);
+            GoogleIdToken.Payload payload = idToken.getPayload();
+
+            String email = payload.getEmail();
+            String firstName = (String) payload.get("given_name");
+            String lastName = (String) payload.get("family_name");
+            String pictureUrl = (String) payload.get("picture");
+
+            userService.registerGoogleUser(email, firstName, lastName, pictureUrl, refreshToken);
+
+            UserDto userDto = userService.googleLogin(email);
+            userDto.setToken(userAuthenticationProvider.createToken(userDto.getEmail()));
+
+            return ResponseEntity.ok(userDto);
+        } catch (Exception e) {
+            // Log the exception
+            System.out.println(e);
+        }
+        return null;
+    }
+}
