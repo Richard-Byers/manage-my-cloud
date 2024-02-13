3bb1dce4c06ced02e4a45b85f5b9f741e8ebdf11
Deividas Dusevicius <deividasdusevicius@yahoo.com>
2024-02-13 13:06:55 +0000

    FE-Navbar Made Responsive

--- manage-my-cloud-frontend/src/components/nav/Navbar.tsx
+++ manage-my-cloud-frontend/src/components/nav/Navbar.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link, useNavigate} from 'react-router-dom';
 import './Navbar.css';
 import logo from '../images/managemycloudlogo.png';
 import {ROUTES} from "../../constants/RouteConstants";
 import {Popover} from "@mui/material";
 import {AuthData} from "../routing/AuthWrapper";
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 const Navbar: React.FC = () => {
-    const {user, logout} = AuthData();
+    const {logout} = AuthData();
     const navigate = useNavigate();
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const [sidebarVisible, setSideBarVisible] = useState(false);
     const open = Boolean(anchorEl);
+
+    const sideBarClass = `sidebar ${sidebarVisible ? 'sidebar-visible' : 'sidebar-hidden'}`;
 
     const handlePopoverOpen = (event: React.MouseEvent<HTMLElement>) => {
         setAnchorEl(event.currentTarget);
-      };
+    };
 
     const handlePopoverClose = () => {
-    setAnchorEl(null);
+        setAnchorEl(null);
     };
+
+    const showSidebar = () => {
+        setSideBarVisible(true);
+    }
+
+    const hideSideBar = () => {
+        setSideBarVisible(false);
+    }
 
     function navigateToProfile() {
         navigate(ROUTES.PROFILE);
@@ -28,56 +41,84 @@ const Navbar: React.FC = () => {
     const profileImage = localStorage.getItem('profileImage') || logo;
 
     return (
-        <nav className="navbar">
-            <li className="logo">
-                <Link to={ROUTES.LANDING} className="nav-link-logo">
-                    <img src={logo} alt="manage my cloud logo" height="100" width="100"/>
-                </Link>
-            </li>
-            <ul className="navbar-nav">
-                <li className="nav-item">
-                    <Link to={ROUTES.DASHBOARD} className="nav-link">
+        <nav className={"nav"}>
+
+            <ul className={sideBarClass}>
+                <li className="nav-item" onClick={hideSideBar}>
+                    <button className={"sidebar-button-close"}><CloseIcon/></button>
+                </li>
+
+                <li>
+                    <img src={profileImage} alt="Profile" className="popover-style"/>
+                </li>
+
+                <li>
+                    <Link to={ROUTES.DASHBOARD}>
                         Dashboard
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link to={ROUTES.MANAGE_CONNECTIONS} className="nav-link">
+                <li>
+                    <Link to={ROUTES.MANAGE_CONNECTIONS}>
                         Manage Connections
                     </Link>
                 </li>
-                <li className="nav-item">
-                    <Link to={ROUTES.PROFILE} className="nav-link">
+                <li>
+                    <Link to={ROUTES.PROFILE}>
                         Profile
                     </Link>
                 </li>
             </ul>
-            <div>
-    <button className="profile-button" onClick={handlePopoverOpen}>
-        <img src={profileImage} alt="Profile" className="popover-style"/>
-    </button>
-    <Popover
-      open={open}
-      anchorEl={anchorEl}
-      onClose={handlePopoverClose}
-      anchorOrigin={{
-        vertical: 'bottom',
-        horizontal: 'center',
-      }}
-      transformOrigin={{
-        vertical: 'top',
-        horizontal: 'center',
-      }}
-    >
-      <div className={'popover-container'}>
-        <button className={'popover-button'} onClick={navigateToProfile}>
-          Profile
-        </button>
-        <button className={'popover-button'} onClick={logout}>
-          Logout
-        </button>
-      </div>
-    </Popover>
-  </div>
+
+            <ul className="navbar-nav">
+                <li className="logo">
+                    <Link to={ROUTES.LANDING} className="nav-link-logo">
+                        <img src={logo} alt="manage my cloud logo" height="100" width="100"/>
+                    </Link>
+                </li>
+                <li className="hide-on-mobile">
+                    <Link to={ROUTES.DASHBOARD} className="nav-link">
+                        Dashboard
+                    </Link>
+                </li>
+                <li className="hide-on-mobile">
+                    <Link to={ROUTES.MANAGE_CONNECTIONS} className="nav-link">
+                        Manage Connections
+                    </Link>
+                </li>
+                <li className="hide-on-mobile">
+                    <Link to={ROUTES.PROFILE} className="nav-link">
+                        Profile
+                    </Link>
+                </li>
+                <li className="nav-item" onClick={showSidebar}>
+                    <button className={"sidebar-button-open"}><MenuIcon/></button>
+                </li>
+                <button className="profile-button" onClick={handlePopoverOpen}>
+                    <img src={profileImage} alt="Profile" className="popover-style"/>
+                </button>
+                <Popover
+                    open={open}
+                    anchorEl={anchorEl}
+                    onClose={handlePopoverClose}
+                    anchorOrigin={{
+                        vertical: 'bottom',
+                        horizontal: 'center',
+                    }}
+                    transformOrigin={{
+                        vertical: 'top',
+                        horizontal: 'center',
+                    }}
+                >
+                    <div className={'popover-container'}>
+                        <button className={'popover-button'} onClick={navigateToProfile}>
+                            Profile
+                        </button>
+                        <button className={'popover-button'} onClick={logout}>
+                            Logout
+                        </button>
+                    </div>
+                </Popover>
+            </ul>
         </nav>
     );
 };
